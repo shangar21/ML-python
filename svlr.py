4c013f382b908e606c24d94613cad09f1fdb7cda
Shangar Muhunthan <ssm2108@localhost.localdomain>
2020-02-26 20:48:00 -0500

    changed alpha

--- svlr.py
+++ svlr.py
@@ -4,10 +4,9 @@ import numpy as np
 def h(t1: float, t2: float, x: float):
 	return t1 + (t2 * x)
 
-def j(t1: float, t2:float, x: np.array, y: np.array, a: int, i: int):
+def j(t1: float, x: np.array, y: np.array, a: int, i: int):
 	m = len(x)
-	t1 = t1 - a*(1/m)*(h(t1, t2, x[i]) - y[i])
-	t2 = t2 - a*(1/m)*(h(t1, t2, x[i]) - y[i])
+	return t1 - a*(1/m)*(h(t1, t2, x[i]) - y[i])
 
 def check_conv(t1, t2):
 	return abs(t1) - abs(t2) < 0.001
@@ -16,18 +15,22 @@ def check_conv(t1, t2):
 t1 = 0
 t2 = 1
 i = 0
-x = np.arange(20)
-y = np.arange(20)
-a = 0.5
-conv = True
+x = np.arange(-10,10)
+y = [k *k for k in x]
+a = .5
+conv = check_conv(t1, t2)
 
 print(x)
 print(y)
 
-while(conv):
-	j(t1, t2, x, y, a, i)
+while(conv and i < len(x)):
+	t1 = j(t1, x, y, a, i)
+	t2 = j(t2, x, y, a, i)
 	conv = check_conv(t1, t2)
+	print(t1, " ,", t2, " ,", abs(t1 - t2))
 	i += 1
 
-print("done!")
+if (conv):
+	print("found min")
+
 
