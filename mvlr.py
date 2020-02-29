f52c3f099dd47d24e81e8aa236802b700dcd096c
--global <--global>
2020-02-28 20:35:56 -0500

    started multivariable linear regression, also added a data sheet to try it out

--- mvlr.py
+++ mvlr.py
@@ -1,3 +1,9 @@
 import numpy as np 
 import matplotlib as mp 
+import svlr.py as svlr
+
+#applying gradient decent to every element in a vector
+
+#we first need a vector x ([x1,x2,x3...xn])
+	#augment said vecor by adding a one at the end
 
