82978200a7f32519ce226af5bcc8aebcf0a2528f
shangar muhunthan <ssm@pop-os.localdomain>
2020-03-01 01:45:33 -0500

    started on the mvlr, still need to find when it converges

--- mvlr.py
+++ mvlr.py
@@ -1,9 +1,24 @@
-import numpy as np 
-import matplotlib as mp 
-import svlr.py as svlr
 
-#applying gradient decent to every element in a vector
+import numpy as np
+import matplotlib.pyplot as plt
+import time 
 
-#we first need a vector x ([x1,x2,x3...xn])
-	#augment said vecor by adding a one at the end
+
+#already added a row of ones at the bottom of the CSV
+x = np.genfromtxt('mlr09.csv', comments=None, delimiter=',', skip_header=1)
+
+y = x[:, 4]
+
+W = np.ones(x.shape[1])
+
+mu = 0.5
+
+deriv = W.T.dot(x.T.dot(x)) - y.T.dot(x)
+
+while():
+	deriv = W.T.dot(x.T.dot(x)) - y.T.dot(x)
+	W = W - mu * deriv
+
+print("Converged!")
+
 
