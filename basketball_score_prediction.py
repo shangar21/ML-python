619d7a8f4eef2f7fccb540d58b41add91994fa86
shangar muhunthan <ssm@pop-os.localdomain>
2020-03-05 17:11:39 -0500

    finally realized why predicted values were so off, fixed

--- basketball_score_prediction.py
+++ basketball_score_prediction.py
@@ -4,14 +4,14 @@ import numpy as np
 x = np.genfromtxt('mlr09.csv', comments=None, delimiter=',', skip_header=1)
 y = x[:, 4]
 x = np.delete(x,4,axis=1)
-x = np.c_[np.ones(x.shape[0]), x]
-
+x = np.c_[x, np.ones(x.shape[0])]
 p_inv = np.linalg.inv(x.T.dot(x))
-
+print(p_inv)
 p_inv = p_inv.dot(x.T).dot(y)
+
 
 height = float(input('enter height in feet (format: feet.inches): '))
 weight = float(input('enter weight in lbs: '))
 fg = float(input('enter field goal percentage: '))
 ft = float(input('enter free throw percentage: '))
-print('predicted average points per game: ', np.array([height, weight, fg, ft]).dot(p_inv.T))
+print('predicted average points per game: ', np.array([height, weight, fg, ft, 1]).dot(p_inv.T))
