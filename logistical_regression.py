4cd1004f75bbf6685b6e3ed7237bfd90d1c129d2
shangar21 <sharanshangar@gmail.com>
2020-03-10 16:00:54 -0400

    finished conv test

--- logistical_regression.py
+++ logistical_regression.py
@@ -1,10 +1,15 @@
 import numpy as np
+import math
 
 def g(x, t):
 	z = x.dot(t)
-	return 1
+	for i in z:
+		z[i] = 1/1 + math.exp(-z[i])
+
+
 
 x = np.genfromtxt('ufwbcd.csv', comments=None, delimiter=',')
+
 # results, benign = 2, malignant = 4
 y = x[:, 0]
 
@@ -14,10 +19,16 @@ x = np.c_[np.ones(x.shape[0]), x]
 a = 0.5
 m = x.shape[1]
 t = np.zeros(x.shape[1])
+dv = np.array()
 conv = False
 
 while not conv:
-	dv = x.T.dot()
+	dv = (x.T.dot((g(x,t)) - y)
+	t = t - (a/m)*(dv)
+	conv = np.vectorize(abs)(dv).sum() <0.0001
+
+
+print("conv!")
 
 
 
