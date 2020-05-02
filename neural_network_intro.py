23ca3be0e12f331fff6a1ad9cbf13b6f278ef4be
shangar21 <sharanshangar@gmail.com>
2020-05-02 14:50:52 -0400

    continued tutorial

--- neural_network_intro.py
+++ neural_network_intro.py
@@ -4,6 +4,24 @@ import torch
 import torchvision
 from torchvision import transforms, datasets
 import matplotlib.pyplot as plt
+import torch.nn as nn #oop
+import torch.nn.functional as F 
+import torch.optim as optim
+
+class Net(nn.Module):
+	def __init__(self):
+		super().__init__()
+		self.fc1 = nn.Linear(28*28, 64)
+		self.fc2 = nn.Linear(64, 64)
+		self.fc3 = nn.Linear(64, 64)
+		self.fc4 = nn.Linear(64, 10)
+
+	def forward(self, x):
+		x = F.relu(self.fc1(x))
+		x = F.relu(self.fc2(x))
+		x = F.relu(self.fc3(x))
+		x = self.fc4(x)
+		return F.log_softmax(x, dim=1)
 
 # fetching data from built in data set from pytorch MNIST (handwritten digits)
 train = datasets.MNIST("", train=True, download=True, transform=transforms.Compose([transforms.ToTensor()])) 
@@ -13,14 +31,32 @@ test = datasets.MNIST("", train=False, download=True, transform=transforms.Compo
 trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)
 testset = torch.utils.data.DataLoader(test, batch_size=10, shuffle=True)
 
-# for data in trainset:
-# 	#print(data)
-# 	break
+net = Net()
 
-print(trainset)
+optimizer = optim.Adam(net.parameters(), lr=0.001)
 
-# plt.imshow(data[0][0].view(28,28)) #apparantly shape of data = (1,28,28).... weird right? so you get errors when trying to output... wonder why that 1 is there by boi left me in suspense
-# plt.show()
+EPOCHS = 3
 
+for epoch in range(EPOCHS):
+	for data in trainset:
+		X, y = data
+		net.zero_grad()
+		output = net(X.view(-1, 28*28))
+		loss = F.nll_loss(output, y)
+		loss.backward()
+		optimizer.step()
 
+correct = 0
+total = 0
+
+with torch.no_grad():
+	 for data in trainset:
+	 	X, y = data
+	 	output = net(X.view(-1,28*28))
+	 	for idx, i in enumerate(output):
+	 		if torch.argmax(i) == y[idx]:
+	 			correct += 1
+	 		total += 1
+
+print("Accuracy: ", correct/total)
 
