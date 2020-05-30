02f41e0bf72f84ef5995a7a717068364d883cb4b
shangar21 <sharanshangar@gmail.com>
2020-05-30 13:51:21 -0400

    needa shit so ill be back

--- k_means_clustering.py
+++ k_means_clustering.py
@@ -1,6 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def get_mean():
+	pass
+
+def move(x, y, kp):
+	pass
+
+def is_conv():
+	pass
+
+
 sz=35
 
 x = np.random.rand(sz)
@@ -10,7 +20,16 @@ k=4
 x_k = np.random.rand(k)
 y_k = np.random.rand(k)
 
+cp = {}
+kp = {0:[], 1:[], 2:[]}
+
+
+
 for i in range(sz):
+	p_dist = []
 	for j in range(k):
-		dist = numpy.linalg.norm(np.array(x[i], y[i]) - np.array(x_k[j], y_k[j]))
-		print(dist)
+		p_dist.append(np.linalg.norm(np.array(x[i], y[i]) - np.array(x_k[j], y_k[j])))
+	cp[i] = p_dist.index(min(p_dist))
+	kp[p_dist.index(min(p_dist))] = kp.get(p_dist.index(min(p_dist)), [])
+	kp[p_dist.index(min(p_dist))].append(i)
+
