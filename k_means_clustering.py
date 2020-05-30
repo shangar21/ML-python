dd00dd7eff13239ea26b35fed57e04bca55246f2
shangar21 <sharanshangar@gmail.com>
2020-05-30 16:07:17 -0400

    asdf

--- k_means_clustering.py
+++ k_means_clustering.py
@@ -1,15 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial import  distance
 
-def get_mean():
-	pass
-
-def move(x, y, kp):
-	pass
-
-def is_conv():
-	pass
-
+def move(kp, x_k, y_k):
+	x_sum = 0
+	y_sum = 0
+	numitems = 0
+	k_dict = {}
+	for i in kp:
+		for j in kp[i]:
+			x_sum += j[0]
+			y_sum += j[1]
+			numitems += 1
+		x_k[i] = x_sum/numitems
+		y_k[i] = y_sum/numitems
 
 sz=35
 
@@ -20,16 +24,32 @@ k=4
 x_k = np.random.rand(k)
 y_k = np.random.rand(k)
 
+plt.scatter(x,y)
+plt.scatter(x_k, y_k)
+plt.show()
+
+
 cp = {}
-kp = {0:[], 1:[], 2:[]}
+kp = {}
 
+conv = False
 
+while not conv:
+	for i in range(sz):
+		p_dist = []
+		for j in range(k):
+			p_dist.append(distance.euclidean((x[i], y[i]) - (x_k[j], y_k[j])))
 
-for i in range(sz):
-	p_dist = []
-	for j in range(k):
-		p_dist.append(np.linalg.norm(np.array(x[i], y[i]) - np.array(x_k[j], y_k[j])))
-	cp[i] = p_dist.index(min(p_dist))
-	kp[p_dist.index(min(p_dist))] = kp.get(p_dist.index(min(p_dist)), [])
-	kp[p_dist.index(min(p_dist))].append(i)
+		min_dist = p_dist.index(min(p_dist))
+		cp[i] = min_dist
+		kp[min_dist] = kp.get(min_dist, [])
+		kp[min_dist].append([x[i], y[i]])
 
+		pre_x, pre_y = x_k, y_k
+		move(kp, x_k, y_k)
+
+		conv = np.array_equiv(pre_x, x_k) and np.array_equiv(y_k, pre_y)
+
+plt.scatter(x,y)
+plt.scatter(x_k, y_k)
+plt.show()
